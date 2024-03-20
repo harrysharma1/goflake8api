6a5de0fac3ea819409faf37e88c549f1e57fa72b
Harry Sharma <harrysharma1066@gmail.com>
2024-03-20 11:04:28 +0000

    feat: add - api function that calls endpoint - json printing function - pretty printing function

--- flake8.go
+++ flake8.go
@@ -10,3 +10,71 @@ import (
 	"os"
 	md "github.com/JohannesKaufmann/html-to-markdown"
 )
+
+type Body struct {
+	Code    string   `json:"code"`
+	Message string   `json:"message"`
+	Content string   `json:"content"`
+	Links   []string `json:"links"`
+}
+
+func api(enable bool, code string)  {
+	client := &http.Client{}
+	endpoint := "https://www.flake8rules.com/api/rules/"+code+"/"
+	req, err := http.NewRequest("GET", endpoint, nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+	bodyText, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if !enable{
+		normalPrint(bodyText)
+	}else{
+		prettyPrint(bodyText)
+	}
+}
+
+
+func normalPrint(text [] uint8) {
+	fmt.Printf("%s\n",text)
+}
+
+func prettyPrint(text[] uint8) {
+	body := Body{}
+	err := json.Unmarshal(text, &body)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	converter := md.NewConverter("", true, nil)
+
+	fmt.Println(body.Code)
+	fmt.Println(body.Message)
+
+	markdown, err:= converter.ConvertString(body.Content)
+
+	if err!=nil{
+		log.Fatal(err)
+	}
+	fmt.Println(markdown)
+	for _,i := range body.Links{
+		fmt.Println(i)
+	}
+}
+
+
+
